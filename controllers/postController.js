78d761f5885ba4888aba59cf7f2d0505308ab11f
philipng88 <philiplyng@gmail.com>
2019-10-13 23:46:58 -0600

    added update post functionality and additional flash messages

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,3 +30,27 @@ exports.viewEditScreen = async function(req, res) {
     res.render("404")
   }
 }
+
+exports.edit = function(req, res) {
+  let post = new Post(req.body, req.visitorId, req.params.id)
+  post.update().then(status => {
+    if (status == "success") {
+      req.flash("success", "Post successfully updated")
+      req.session.save(function() {
+        res.redirect(`/post/${req.params.id}/edit`)
+      })
+    } else {
+      post.errors.forEach(function(error) {
+        req.flash("errors", error)
+      })
+      req.session.save(function() {
+        res.redirect(`/post/${req.params.id}/edit`)
+      })
+    }
+  }).catch(() => {
+    req.flash("errors", "You do not have permission to perform that action")
+    req.session.save(function() {
+      res.redirect("/")
+    })
+  })
+}
