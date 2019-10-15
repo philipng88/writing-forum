4ce693f019bc1edbac59f9d89496d954bf2154c0
philipng88 <philiplyng@gmail.com>
2019-10-14 23:42:16 -0600

    added functionality to stop following a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
 const User = require('../models/User')
 const Post = require('../models/Post')
+const Follow = require('../models/Follow')
+
+exports.sharedProfileData = async function(req, res, next) {
+  let isVisitorsProfile = false
+  let isFollowing = false
+  if (req.session.user) {
+    isVisitorsProfile = req.profileUser._id.equals(req.session.user._id)
+    isFollowing = await Follow.isVisitorFollowing(req.profileUser._id, req.visitorId)
+  }
+  req.isVisitorsProfile = isVisitorsProfile
+  req.isFollowing = isFollowing
+  next()
+}
 
 exports.mustBeLoggedIn = function(req, res, next) {
   if (req.session.user) {
@@ -71,10 +84,11 @@ exports.profilePostsScreen = function(req, res) {
   Post.findByAuthorId(req.profileUser._id).then(function(posts) {
     res.render('profile', {
       posts: posts,
-      profileUsername: req.profileUser.username
+      profileUsername: req.profileUser.username,
+      isFollowing: req.isFollowing,
+      isVisitorsProfile: req.isVisitorsProfile
     })
   }).catch(function() {
     res.render("404")
   })
-
 }
--- models/Follow.js
+++ models/Follow.js
@@ -14,19 +14,33 @@ Follow.prototype.cleanup = function() {
     }
 }
 
-Follow.prototype.validate = async function() {
+Follow.prototype.validate = async function(action) {
     let followedAccount = await usersCollection.findOne({username: this.followedUsername})
     if (followedAccount) {
         this.followedId = followedAccount._id
     } else {
         this.errors.push("You cannot follow a user that does not exist")
     }
+    let doesFollowAlreadyExist = await followsCollection.findOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+    if (action == "create") {
+        if (doesFollowAlreadyExist) {
+            this.errors.push("You are already following this user")
+        }
+    }
+    if (action == "delete") {
+        if (!doesFollowAlreadyExist) {
+            this.errors.push("You are already not following this user")
+        }
+    }
+    if (this.followedId.equals(this.authorId)) {
+        this.errors.push("You cannot follow yourself")
+    }
 }
 
 Follow.prototype.create = function() {
     return new Promise(async (resolve, reject) => {
         this.cleanup()
-        await this.validate()
+        await this.validate("create")
         if (!this.errors.length) {
             await followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
             resolve()
@@ -34,6 +48,28 @@ Follow.prototype.create = function() {
             reject(this.errors)
         }
     })
+}
+
+Follow.prototype.delete = function() {
+    return new Promise(async (resolve, reject) => {
+        this.cleanup()
+        await this.validate("delete")
+        if (!this.errors.length) {
+            await followsCollection.deleteOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
+}
+
+Follow.isVisitorFollowing = async function(followedId, visitorId) {
+    let followDoc = await followsCollection.findOne({followedId, authorId: new ObjectID(visitorId)})
+    if (followDoc) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = Follow
